6fbb2504da33307b4894ecb26e05a590bce86e9c
sukriti-sigh13 <sukriti.singh899@gmail.com>
2022-06-22 14:30:34 +0530

    functionality-added

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,17 +2,40 @@ import { Tabs, Tab } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaFire, FaGem } from "react-icons/fa";
 import { GiByzantinTemple } from "react-icons/gi";
+import Fade from 'react-bootstrap/Fade'
 import Card from "./Cards";
 import "../index.css";
 import data from "../data";
 
+
+
 export default function TabsCustom() {
-  const CardElement = data.map((item) => {
+  const RecentCardElement = data.data1.map((item) => {
     return <Card key={item.id} {...item} />;
   });
+  const LatestShowCardElement = data.data2.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+  const PopularCardElement = data.data3.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+  const GenisisCardElement = data.data4.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+  const InTempleCardElement = data.data5.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+  const InVoidCardElement = data.data6.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+  const BAYCCardElement = data.data7.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
   return (
     <Tabs defaultActiveKey="24h-trending" className="my-5">
       <Tab
+        transition={false}
         eventKey="24h-trending"
         title={
           <span>
@@ -21,15 +44,20 @@ export default function TabsCustom() {
           </span>
         }
       >
-        <div className="sectionstyle">{CardElement}</div>
+        <div className="sectionstyle">{RecentCardElement}</div>
       </Tab>
-      <Tab eventKey="Latest shows" title="Latest shows">
-        <div className="sectionstyle">{CardElement}</div>
+      <Tab 
+        transition={false}
+        eventKey="Latest shows" title="Latest shows">
+        <div className="sectionstyle">{LatestShowCardElement}</div>
       </Tab>
-      <Tab eventKey="Most Popular" title="Most Popular">
-        <div className="sectionstyle">{CardElement}</div>
+      <Tab 
+      transition={false}
+       eventKey="Most Popular" title="Most Popular">
+        <div className="sectionstyle">{PopularCardElement}</div>
       </Tab>
       <Tab
+      transition={false}
         eventKey="In genisis"
         title={
           <span>
@@ -38,9 +66,10 @@ export default function TabsCustom() {
           </span>
         }
       >
-        <div className="sectionstyle">{CardElement}</div>
+        <div className="sectionstyle">{GenisisCardElement}</div>
       </Tab>
       <Tab
+      transition={false}
         eventKey="In Temple"
         title={
           <span>
@@ -49,13 +78,17 @@ export default function TabsCustom() {
           </span>
         }
       >
-        <div className="sectionstyle">{CardElement}</div>
+        <div className="sectionstyle">{InTempleCardElement}</div>
       </Tab>
-      <Tab eventKey="In void" title="In void">
-        <div className="sectionstyle">{CardElement}</div>
+      <Tab 
+      transition={false}
+      eventKey="In void" title="In void">
+        <div className="sectionstyle">{ InVoidCardElement}</div>
       </Tab>
-      <Tab eventKey="#BAYC" title="#BAYC">
-        <div className="sectionstyle">{CardElement}</div>
+      <Tab 
+      transition={false}
+      eventKey="#BAYC" title="#BAYC">
+        <div className="sectionstyle">{BAYCCardElement}</div>
       </Tab>
     </Tabs>
   );
--- src/data.js
+++ src/data.js
@@ -1,76 +1,431 @@
-export default[
+export default{
+  data1:[
     {
-        id:1,
-        image:"/img/image1.jpg",
-        heading:"Meta Trap house",
-        info:{
-          name:"@freee23le..",
-          address:"0xBBc2kko"
-        },
-        left:1
+      id:1,
+      image:"/img/image1.jpg",
+      heading:"Meta Trap house",
+      info:{
+        name:"@freee23le..",
+        address:"0xBBc2kko"
+      },
+      left:1
 
-    },
-    {
-        id:2,
-        image:"/img/image2.jpg",
-        heading:"The Vincent Van Dough Gallery",
-        info:{
-          name:"@suk2389..",
-          address:"0xdmls"
-        },
-        left:2,
-        secondinfo:{
-            name1:"@dfhks999",
-            address1:"0x4dd08"
-        }
-    },
-    {
-        id:3,
-        image:"/img/image3.jpg",
-        heading:"Teufzar",
-        info:{
-          name:"@kiylieh..",
-          address:"0x45893b"
-        },
-        left:1
+  },
+  {
+      id:2,
+      image:"/img/image2.jpg",
+      heading:"The Vincent Van Dough Gallery",
+      info:{
+        name:"@suk2389..",
+        address:"0xdmls"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@dfhks999",
+          address1:"0x4dd08"
+      }
+  },
+  {
+      id:3,
+      image:"/img/image3.jpg",
+      heading:"Teufzar",
+      info:{
+        name:"@kiylieh..",
+        address:"0x45893b"
+      },
+      left:1
+    
+  },
+  {
+      id:4,
+      image:"/img/image4.jpg",
+      heading:"6529 AB+1",
+      info:{
+        name:"@rskuj..",
+        address:"0xBBrr2bno"
+      },
+      left:1,
       
-    },
+      
+  },
+  {
+      id:5,
+      image:"/img/image5.webp",
+      heading:"#9049",
+      info:{
+        name:"@0x02s..",
+        address:"0xff7o"
+      },
+      left:1
+      
+  },
+  {
+      id:6,
+      image:"/img/image6.webp",
+      heading:"6529 Photo A",
+      info:{
+        name:"@call65..",
+        address:"0xBBc345s"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@yu4hf99",
+          address1:"0x2334d08"
+      }
+      
+  }
+  ],
+  data2:[
     {
-        id:4,
-        image:"/img/image4.jpg",
-        heading:"6529 AB+1",
-        info:{
-          name:"@rskuj..",
-          address:"0xBBrr2bno"
-        },
-        left:1,
-        
-        
+      id:1,
+      image:"/img/image-21.webp",
+      heading:"Dogecoin-35 house",
+      info:{
+        name:"@fredhsuwwle..",
+        address:"0xrfu22kko"
+      },
+      left:1
+
+  },
+  {
+      id:2,
+      image:"/img/image-22.webp",
+      heading:"The Art Gallery",
+      info:{
+        name:"@gevk2389..",
+        address:"0xdmls"
+      },
+      left:1
+  },
+  {
+      id:3,
+      image:"/img/image-23.webp",
+      heading:"Artistic Alpha",
+      info:{
+        name:"@yagami",
+        address:"0x4590b"
+      },
+      left:1
+    
+  },
+  {
+    id:4,
+    image:"/img/image-24.webp",
+    heading:"Secret Stones",
+    info:{
+      name:"@phantomtroupe",
+      address:"0x4590b"
     },
+    left:1
+  
+},
+{
+  id:5,
+  image:"/img/image-51.avif",
+  heading:"Bling Bits",
+  info:{
+    name:"@zocizr..",
+    address:"0x4590b"
+  },
+  left:2,
+  secondinfo:{
+      name1:"@hjud45op",
+      address1:"0x34fv8"
+  }
+
+}
+  ],
+  data3:[
     {
-        id:5,
-        image:"/img/image5.webp",
-        heading:"#9049",
-        info:{
-          name:"@0x02s..",
-          address:"0xff7o"
-        },
-        left:1
-        
-    },
+      id:1,
+      image:"/img/image-31.avif",
+      heading:"Cryptokitty Ville",
+      info:{
+        name:"@bhamastra",
+        address:"0xRHUIkko"
+      },
+      left:1
+
+  },
+  {
+      id:2,
+      image:"/img/image-32.avif",
+      heading:"Blizz Blizzard",
+      info:{
+        name:"@respect.",
+        address:"0xdmls"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@dfhks999",
+          address1:"0x4dd08"
+      }
+  },
+  {
+      id:3,
+      image:"/img/image-54.avif",
+      heading:"To The Moon",
+      info:{
+        name:"@heymons",
+        address:"0x66673b"
+      },
+      left:1
+    
+  },
+  {
+      id:4,
+      image:"/img/image-34.avif",
+      heading:"Koala Kickers",
+      info:{
+        name:"@kile..",
+        address:"0xBBrr2bno"
+      },
+      left:1,
+      
+      
+  },
+  {
+      id:5,
+      image:"/img/image-35.avif",
+      heading:"#9049",
+      info:{
+        name:"@0x02s..",
+        address:"0xff7o"
+      },
+      left:1
+      
+  },
+  {
+      id:6,
+      image:"/img/image1.jpg",
+      heading:"Dose of Art",
+      info:{
+        name:"@call65..",
+        address:"0xkjEEFG345s"
+      },
+      left:1
+      
+  }
+  ],
+  data4:[
     {
-        id:6,
-        image:"/img/image6.webp",
-        heading:"6529 Photo A",
-        info:{
-          name:"@call65..",
-          address:"0xBBc345s"
-        },
-        left:2,
-        secondinfo:{
-            name1:"@yu4hf99",
-            address1:"0x2334d08"
-        }
-        
-    },
-]
+      id:1,
+      image:"/img/image-43.avif",
+      heading:"Dust Rift",
+      info:{
+        name:"@freee23le..",
+        address:"0xBBc2kko"
+      },
+      left:1
+
+  },
+  {
+      id:2,
+      image:"/img/image-42.avif",
+      heading:"1NiTRO",
+      info:{
+        name:"@nonstopmusic..",
+        address:"0KLLLxhj"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@1999345RXY",
+          address1:"0xklsch89-2"
+      }
+  },
+  {
+      id:3,
+      image:"/img/image-64.avif",
+      heading:"TouchDown",
+      info:{
+        name:"@space",
+        address:"0x201124X"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@zzzzzz",
+          address1:"0xkildfgd08"
+      }
+    
+  },
+  {
+      id:4,
+      image:"/img/image-44.avif",
+      heading:"Light Embrace",
+      info:{
+        name:"@rskuj..",
+        address:"0xBBrr2bno"
+      },
+      left:1,
+      
+      
+  },
+  {
+      id:5,
+      image:"/img/image-45.avif",
+      heading:"#PhantomTrop",
+      info:{
+        name:"@0x02s..",
+        address:"0xff7o"
+      },
+      left:1
+      
+  },
+  {
+      id:6,
+      image:"/img/image-31.avif",
+      heading:"AZ34 Photo X",
+      info:{
+        name:"@call65..",
+        address:"0xBBc345s"
+      },
+     left:1
+      
+  }
+  ],
+  data5:[
+    {
+      id:1,
+      image:"/img/image-51.avif",
+      heading:"3D Me",
+      info:{
+        name:"@tommy",
+        address:"0xshelbies33"
+      },
+      left:1
+
+  },
+  {
+      id:2,
+      image:"/img/image3.jpg",
+      
+      heading:"0xHile",
+      info:{
+        name:"@hely..",
+        address:"0xhyda"
+      },
+      left:1
+  },
+  {
+      id:3,
+      image:"/img/image-52.avif",
+      heading:"Block View",
+      info:{
+        name:"@kardashianscryp..",
+        address:"0xSILb"
+      },
+      left:1
+    
+  }],
+  data6:[
+    {
+      id:1,
+      image:"/img/image-61.avif",
+      heading:"0NOCH0",
+      info:{
+        name:"@grace",
+        address:"0xBBc2kko"
+      },
+      left:1
+
+  },
+  {
+      id:2,
+      image:"/img/image-62.avif",
+      heading:"Crumbs NFT",
+      info:{
+        name:"@veleXy..",
+        address:"0xzooes"
+      },
+      left:1
+  },
+  {
+      id:3,
+      image:"/img/image-63.avif",
+      heading:"Enforcers",
+      info:{
+        name:"@kiylieh..",
+        address:"0x4FdSS"
+      },
+      left:1
+    
+  },
+  {
+      id:4,
+      image:"/img/image-64.avif",
+      heading:"0N0TES",
+      info:{
+        name:"@ghiusdf.",
+        address:"0xbn9990bno"
+      },
+      left:1,
+      
+      
+  },
+  {
+      id:5,
+      image:"/img/image-35.avif",
+      heading:"#1312204",
+      info:{
+        name:"@0x02s..",
+        address:"0xff7o"
+      },
+      left:1
+      
+  },
+  {
+      id:6,
+      image:"/img/image-24.webp",
+      heading:"Volcanos",
+      info:{
+        name:"@address.com.",
+        address:"0x23rh4s"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@yu4hf99",
+          address1:"0x2334d08"
+      }
+      
+    }
+  ],
+  data7:[
+    {
+      id:1,
+      image:"/img/image-23.webp",
+      heading:"0r0ckR",
+      info:{
+        name:"@kanizxtane..",
+        address:"0xBBc2kko"
+      },
+      left:1
+
+  },
+  {
+      id:2,
+      image:"/img/image-34.avif",
+      heading:"ArtPlex",
+      info:{
+        name:"@codesXy",
+        address:"0xdmls"
+      },
+      left:2,
+      secondinfo:{
+          name1:"@33312",
+          address1:"0xgone"
+      }
+  },
+  {
+      id:3,
+      image:"/img/image5.webp",
+      heading:"Meebyte",
+      info:{
+        name:"@zzzmusk",
+        address:"0xtesss"
+      },
+      left:1
+    
+  }]
+}
+
+    
